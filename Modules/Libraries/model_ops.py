0ec414200816bc5c30a0f93c07282c53ed167eeb
Simone Battistini <66021071+Sim98B@users.noreply.github.com>
2023-11-09 16:04:18 +0100

    Update model_ops.py

--- Modules/Libraries/model_ops.py
+++ Modules/Libraries/model_ops.py
@@ -2,6 +2,51 @@ import torch
 from tqdm.auto import tqdm
 from sklearn.metrics import accuracy_score, f1_score
 
+def eval_step(model,
+              dataloader: torch.utils.data.DataLoader,
+              loss_function: torch.nn.Module,
+              metric: str,
+              device: torch.device):
+  
+  """
+  Performs a single evaluation step on a given model.
+
+  Args:
+    model (torch.nn.Module): model to train
+    dataloader (torch.utils.data.DataLoader): a dataloader containing train data
+    loss_function (torch.nn.Module): a loss function used to track model's performance
+    metric (str): 'accuracy' or 'f1' -> sklearn.metrics is used
+    device (torch.device): 'cpu' or 'cuda', where to train the model
+
+  Returns:
+    Test loss and test matric values
+  """
+
+  metric_dict = {"accuracy":accuracy_score,
+                 "f1":f1_score}
+
+  model.to(device)
+  model.eval()
+
+  test_loss, test_metric = 0, 0
+
+  with torch.inference_mode():
+    for batch, (X, y) in enumerate(tqdm(dataloader, desc = "Evaluating: ")):
+      X, y = X.to(device), y.to(device)
+      y_pred_logits = model(X)
+      loss = loss_function(y_pred_logits, y)
+      test_loss += loss.item() 
+      y_pred_labels = y_pred_logits.argmax(dim=1)
+      if metric == "f1" and len(np.unique(y)):
+        test_metric += metric_dict[metric](y.detach().numpy(), y_pred_labels.detach().numpy(), average = "weighted")
+      else:
+        test_metric += metric_dict[metric](y.detach().numpy(), y_pred_labels.detach().numpy())
+
+  test_loss = test_loss / len(dataloader)
+  test_metric = test_metric / len(dataloader)
+
+  return test_loss, test_metric
+
 def train_step(model,
                dataloader: torch.utils.data.DataLoader,
                loss_function: torch.nn.Module,
