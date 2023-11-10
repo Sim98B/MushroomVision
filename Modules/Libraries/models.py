b9d1f1b5b2a45aeb96785fa47745a5edf4b6ab79
Simone Battistini <66021071+Sim98B@users.noreply.github.com>
2023-11-10 15:43:59 +0100

    Update models.py

--- Modules/Libraries/models.py
+++ Modules/Libraries/models.py
@@ -1,1 +1,54 @@
+import torch
+import torch.nn as nn
 
+class TinyVGG(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*16*16,
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
+
+def create_tiny_vgg(input_shape: int, hidden_units: int, output_shape: int):
+    """
+    Create an instance of TinyVGG.
+
+    Args:
+        input_shape (int): Number of color channels.
+        hidden_units (int): Number of hidden units.
+        output_shape (int): Output shape of the network.
+
+    Returns:
+        TinyVGG: An instance of the TinyVGG model.
+    """
+    model = TinyVGG(input_shape, hidden_units, output_shape)
+    return model
