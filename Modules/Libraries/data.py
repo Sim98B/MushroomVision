ce3393b5a3cb71fa33a66b4aced57464d93725f9
Simone Battistini <66021071+Sim98B@users.noreply.github.com>
2023-11-12 13:16:36 +0100

    Update data.py

--- Modules/Libraries/data.py
+++ Modules/Libraries/data.py
@@ -50,45 +50,6 @@ def dataloader(data_dir: str,
                       pin_memory = memory)
   
   return loader, class_names
-
-def download_data(source: str,
-                  remove_source: bool = True) -> Path:
-  """Downloads a zipped dataset from source and unzips to destination.
-
-    Args:
-        source (str): A link to a zipped file containing data.
-        remove_source (bool): Whether to remove the source after downloading and extracting.
-    
-    Returns:
-        pathlib.Path to downloaded data.
-    
-    Example usage:
-        download_data(source="https://link/to/file.zip")
-    """
-  data_path = Path("data/")
-
-  if data_path.is_dir():
-    print(f"[INFO] {data_path} directory exists.")
-  else:
-    print(f"[INFO] Did not find {data_path} directory, creating one...")
-    data_path.mkdir(parents=True, exist_ok=True)
-
-  target_file = Path(source).name
-  with open(data_path / target_file, "wb") as f:
-    request = requests.get(source)
-    print(f"[INFO] Downloading {target_file} from {source}...")
-    f.write(request.content)
-
-  with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
-    print(f"[INFO] Unzipping {target_file} data...") 
-    zip_ref.extractall(data_path)
-
-  if remove_source:
-    os.remove(data_path / target_file)
-
-  data_path = data_path.joinpath(os.listdir(data_path)[0])
-  
-  return data_path
     
 def inspect_dir(dir_path: Path):
     """
