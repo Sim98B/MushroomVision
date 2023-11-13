17f3e3dc349db675aca4e33bab370212be9fce06
Simone Battistini <66021071+Sim98B@users.noreply.github.com>
2023-11-13 20:18:37 +0100

    Update utils.py

--- Modules/Libraries/utils.py
+++ Modules/Libraries/utils.py
@@ -212,7 +212,7 @@ def save_model(model: torch.nn.Module,
   target_dir_path = Path(target_dir)
   target_dir_path.mkdir(parents=True, exist_ok=True)
 
-  assert model_name.endswith(".pth") or model_name.endswith(".pt"),
+  assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
   model_save_path = target_dir_path / model_name
 
   print(f"[INFO] Saving model to: {model_save_path}")
